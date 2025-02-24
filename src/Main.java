5579b489c4ddba2349fc9842285faf509755f6a0
Mark Barnes <barnemar002@warren.k12.in.us>
2025-02-24 13:47:55 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        GCDCalculator gcd = (a, b) -> {
+            while (b != 0){
+                int oldB = b;
+                b = a % b;
+                a = oldB;
+
+            }
+            return a;
+        };
+
+        System.out.println(gcd.gcd(15, 20));
+
+    }
+
+
+    @FunctionalInterface
+    public interface GCDCalculator{
+        int gcd(int a, int b);
+    }
 }
